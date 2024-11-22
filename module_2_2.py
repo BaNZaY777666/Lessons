92a971f51e58d6ff51ac15a3cbd6c4b98d99b342
BaNZaY777666 <smilik89@mail.ru>
2024-11-22 15:44:00 +0300

    Условная конструкция. Операторы if, elif, else

---
+++ module_2_2.py
@@ -0,0 +1,14 @@
+numbers = set()
+numbers.add(input('Введите первое число: '))
+numbers.add(input('Введите второе число: '))
+numbers.add(input('Введите третье число: '))
+one = (len(numbers))
+if one == 3:
+    print(0)
+elif one == 2:
+    print(2)
+elif one == 1:
+    print(3)
+
+
+
