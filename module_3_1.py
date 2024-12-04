39fff295b34b6bfe75a9f727efb876486f5e70b2
BaNZaY777666 <smilik89@mail.ru>
2024-12-04 11:30:59 +0300

    Домашняя работа по уроку "Пространство имён"

---
+++ module_3_1.py
@@ -0,0 +1,31 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    a = len(string)
+    stringU = string.upper()
+    stringL = string.lower()
+    string_tuple = (a, stringU, stringL)
+    return string_tuple
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string.lower()
+    list_to_search = [i.lower() for i in list_to_search]
+    if string.lower() in list_to_search:
+        print(True)
+    else:
+        print(False)
+
+
+print(string_info('Capibara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
