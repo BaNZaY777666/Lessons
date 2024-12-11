e7664e7d844fbb2404b2fc07ae8bc97137bb2409
BaNZaY777666 <smilik89@mail.ru>
2024-12-11 15:38:03 +0300

    Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+from fake_math import divide as fkm
+from true_math import divide as trm
+
+
+
+result1 = fkm(69, 3)
+result2 = fkm(3, 0)
+result3 = trm(49, 7)
+result4 = trm(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
